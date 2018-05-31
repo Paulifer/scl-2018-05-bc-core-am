274da8cd48eb69972b2f1c53494c0cc6386ec30d
Paulifer <pauli.hua.timm@gmail.com>
2018-05-31 02:12:03 -0400

    arreglando identacion

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@
 /*aca va dom*/
 
 window.onload = function() {
-// Botón para la función de cifrar.
-const btnCifrar = document.getElementById('btnCifrar');
-btnCifrar.addEventListener("click", function (){
-  let text = document.getElementById("boxText").value; 
-  let displacement = document.getElementById("boxNum").value; 
-  window.cipher.encode(text, displacement); 
+// Botón Codificar
+	const btnCifrar = document.getElementById('btnCifrar');
+	btnCifrar.addEventListener("click", function (){
+  	let text = document.getElementById("boxText").value; 
+  	let displacement = document.getElementById("boxNum").value; 
+  	window.cipher.encode(text, displacement); 
 });
 
 
-// Botón para la función  descifrar.
-const btnDescifrar = document.getElementById('btnDescifrar');
-btnDescifrar.addEventListener("click", function (){
-  let text = document.getElementById("boxText").value; 
-  let displacement = document.getElementById("boxNum").value; 
-  window.cipher.encode(text, displacement); 
+// Botón Decodificar
+	const btnDescifrar = document.getElementById('btnDescifrar');
+	btnDescifrar.addEventListener("click", function (){
+  	let text = document.getElementById("boxText").value; 
+  	let displacement = document.getElementById("boxNum").value; 
+  	window.cipher.encode(text, displacement); 
   });
 }
