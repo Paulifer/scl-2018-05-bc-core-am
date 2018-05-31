0e8bad7c14e9c8da4385a136f6db1581565cd6f3
Paulifer <pauli.hua.timm@gmail.com>
2018-05-31 10:05:21 -0400

    agrege la funcion codificar y funciona

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,19 @@
 /*aca va codifico y decofico*/
 window.cipher = {
-  encode: () => {
+  encode: (text,displacement) => {
     /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
+    let textEncode = "";
+  //recorriendo el texto  
+  for(let i = 0; i<text.length; i++){
+  	if (text.charCodeAt(i) === 32) /*32 es el espacio en codigo ascii, es para que lo cuente*/{
+  		textEncode += ' ';
+  	}else{
+  	let textCharCode = (text.charCodeAt(i) - 65 + parseInt(displacement))%26 + 65;
+  	textEncode += String.fromCharCode(textCharCode);
+  	}
+    }
+ 
+document.getElementById('boxNewText').innerHTML = textEncode;
+}
+  
 }
